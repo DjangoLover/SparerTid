ac536260c76e2180d5bfb87694de246bbff3a48c
Maria Yakimova <maria.yakimova@djangostars.com>
2013-09-29 13:45:34 +0300

    add project app

--- sparertid/urls.py
+++ sparertid/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from sparertid.apps import project
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'sparertid.apps.imbot.views.index', name='home'),
+    #url(r'^$', 'sparertid.apps.imbot.views.index', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^project/', include(project.urls)),
 )
